7efa34ebf6918c7f768a64faf60f87945c4e8825
hoxtonhero <dima23mvp@gmail.com>
2017-07-31 19:07:05 +0300

    My moneybox 1st day

--- app/components/App.js
+++ app/components/App.js
@@ -1,14 +1,33 @@
 const React = require('react');
-const RadioGroup = require('./RadioGroup');
+const StartScreen = require('./StartScreen');
+const MainScreen = require('./MainScreen');
 
-const option = {
-  espresso: 'Espresso',
-  americano: 'Americano',
-  latte: 'Latte',
-  cappuccino: 'Capuccino',
-  mocha: 'Mocha',
-  'flat-white': 'Flat white',
-  tea: 'Black tea'
+const json = {
+  title: "Моя Копилка – до 6,75%",
+  caption: "Накопительный счёт с процентом на остаток",
+  tariffUrl: "https://www.open.ru/deposits/my_moneybox",
+  defaultAmount: {
+    RUR: 50000,
+    USD: 300,
+    EUR: 300
+  },
+  interestPercentage: {
+    RUR: {
+      "0": 3,
+      "10000": 5.5,
+      "50000": 5.75
+    },
+    USD: {
+      "0": 0,
+      "300": 0.1,
+      "30000": 0.2
+    },
+    EUR: {
+      "0": 0,
+      "300": 0.1,
+      "30000": 0.2
+    }
+  }
 }
 
 class App extends React.Component {
@@ -16,22 +35,38 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 'no value'
+      mainScr: false,
+      currency: 'rur'
     };
-    this.changeValue = this.changeValue.bind(this);
-  }
-
-  changeValue(newValue){
-    this.setState({
-      value: newValue
-    });
+    this.addMainScreen = this.addMainScreen.bind(this);
+    this.setCurrency = this.setCurrency.bind(this);
   }
     
+  addMainScreen() {
+    if(this.state.mainScr) {
+      this.setState({
+        mainScr: false
+      });
+    } else {
+      this.setState({
+        mainScr: true
+      });
+    }
+  }
+
+  setCurrency(newCur) {
+    this.setState({
+      currency: newCur
+    })
+  }
 
 	render() {
    
 		return (
-        <RadioGroup value = {this.state.value} onChange = {this.changeValue} options = {option} />
+      <div>
+        <StartScreen json = {json} onChange = {this.addMainScreen} mainScr = {this.state.mainScr}/>
+        <MainScreen json = {json} mainScr = {this.state.mainScr} currency = {this.state.currency} setCurrency = {this.setCurrency} />
+      </div>
 		);
     
 	};
---
+++ app/components/StartScreen.js
@@ -0,0 +1,76 @@
+const React = require('react');
+
+const outerStyle1 = {
+    height: 50,
+    width: 50,
+	  borderColor: 'deepskyblue',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderRadius: '50%',
+	  verticalAlign: 'text-top',
+	  'margin-left': 50,
+  }
+
+const outerStyle2 = {
+    height: 50,
+    width: 50,
+    borderColor: 'gainsboro',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderRadius: '50%',
+    verticalAlign: 'text-top',
+    'margin-left': 50,
+  }
+
+const plusStyle = {
+    fontSize: 30, 
+    'margin-left': 16, 
+    'margin-top': 5, 
+    color: 'deepskyblue'
+}
+const crossStyle = {
+    fontSize: 30, 
+    'margin-left': 16, 
+    'margin-top': 5, 
+    color: 'gainsboro'
+}
+
+
+class StartScreen extends React.Component {
+
+  constructor(props) {
+  	super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+  	this.props.onChange();
+  }
+
+  render() {
+
+    const newStyle = (!this.props.mainScr) ? plusStyle : crossStyle;
+    const symbol = (!this.props.mainScr) ? '+' : '×';
+    const outerStyle = (!this.props.mainScr) ? outerStyle1 : outerStyle2;
+
+	  return (
+		  <div> 
+        <div style = {{display: 'inline-block', 'margin-left': 30}}>
+		      <div style = {{fontSize: 20}}>
+		        {this.props.json.title}
+          </div>
+          <div style = {{fontSize: 10, color: 'lawngreen'}}>
+            {this.props.json.caption}
+          </div>
+        </div>
+        <div style = {{display: 'inline-block'}}>
+		      <div style = {outerStyle} onClick = {this.handleClick}> 
+		          <div style = {newStyle}>{symbol}</div>
+		      </div>
+        </div>
+		  </div>
+	  );  
+  };
+}
+
+module.exports = StartScreen;
