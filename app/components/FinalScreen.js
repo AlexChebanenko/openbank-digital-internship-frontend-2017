10bb9bf4ba98a0c69566e468c1b5601e18a475f2
hoxtonhero <dima23mvp@gmail.com>
2017-08-03 18:57:01 +0300

    Spaces

--- app/components/FinalScreen.js
+++ app/components/FinalScreen.js
@@ -1,31 +1,31 @@
 const React = require('react');
 
 const buttonStyle = {
-	'margin-left': 30,
-	width: 150,
-	height: 50,
-	color: 'white',
-	'background-color': 'deepskyblue'
+  'margin-left': 30,
+  width: 150,
+  height: 50,
+  color: 'white',
+  'background-color': 'deepskyblue'
 };
 
 class FinalScreen extends React.Component {
 
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	render() {
-		return (
-			<div style = {{'margin-left': 300, 'margin-top': 300}}>
-				<div> Галочка </div>
-				<div> {this.props.amount}</div>
-				<div> Открытие накопительного счета </div>
-			  <div>
-		      <input type = 'button' style = {buttonStyle} onClick = {this.props.isDone} value = "Готово" />
-		    </div>
-		  </div>
-		);
-	}
+  render() {
+    return (
+      <div style = {{'margin-left': 300, 'margin-top': 300}}>
+        <div> Галочка </div>
+        <div> {this.props.amount}</div>
+        <div> Открытие накопительного счета </div>
+        <div>
+          <input type = 'button' style = {buttonStyle} onClick = {this.props.isDone} value = "Готово" />
+        </div>
+      </div>
+    );
+  }
 }
 
 module.exports = FinalScreen
