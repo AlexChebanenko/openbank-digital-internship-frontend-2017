3bd953a4a5606a4e483a4144ed686cc8c72ee1bb
hoxtonhero <dima23mvp@gmail.com>
2017-08-07 17:06:28 +0300

    trying css

--- app/components/FinalScreen.js
+++ app/components/FinalScreen.js
@@ -1,13 +1,5 @@
 const React = require('react');
 
-const buttonStyle = {
-  'margin-left': 30,
-  width: 150,
-  height: 50,
-  color: 'white',
-  'background-color': 'deepskyblue'
-};
-
 class FinalScreen extends React.Component {
 
   constructor(props) {
@@ -21,7 +13,7 @@ class FinalScreen extends React.Component {
         <div> {this.props.amount}</div>
         <div> Открытие накопительного счета </div>
         <div>
-          <input type = 'button' style={buttonStyle} onClick = {this.props.isDone} value = "Готово" />
+          <input type = 'button' className = 'buttonStyle' onClick = {this.props.isDone} value = "Готово" />
         </div>
       </div>
     );
